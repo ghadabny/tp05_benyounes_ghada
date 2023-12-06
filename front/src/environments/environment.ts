dc44c30afda71057546112086d8179cbe9a77d59
Ghada Ben Younes <ghadabenyounes7@gmail.com>
2023-12-06 19:39:55 +0100

    Ajout Frontend et Backend

---
+++ front/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  backendLoginClient: 'https://tp05-9mfs.onrender.com/api/utilisateur/login',
+  backendCatalogue: 'https://tp05-9mfs.onrender.com/api/catalogue',
+};
